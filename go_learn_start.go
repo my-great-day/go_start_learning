a66b9784244e538579760c9bc477ed28bd8f1e5e
Umar <99081017+my-great-day@users.noreply.github.com>
2022-08-02 12:08:48 +0500

    Update go_learn_start.go

--- go_learn_start.go
+++ go_learn_start.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var wee string
-	wee = "wee"
-
-	fmt.Println(wee)
-
+	fmt.Println("Wee!")
+	for i := 1; i <= 10; i++ {
+		for j := 1; j<=10; j++ {
+			fmt.Print(i * j, "\t")
+		}
+		fmt.Println()
+	}
 }
